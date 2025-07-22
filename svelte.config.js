2f837b202a0aeb34b91f32441d23f122bddb9f43
jngu-mac <jngu@jngu-macui-MacBookAir.local>
2025-07-22 11:55:11 +0900

    update

--- svelte.config.js
+++ svelte.config.js
@@ -41,10 +41,10 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
-		paths: {
-		  // GitHub 저장소 이름으로 변경하세요.
-		  base: process.env.NODE_ENV === 'production' ? '/' : '',
-		},
+		// paths: {
+		//   // GitHub 저장소 이름으로 변경하세요.
+		//   base: process.env.NODE_ENV === 'production' ? '/' : '',
+		// },
 		prerender: {
 		  handleHttpError: 'warn', // 또는 'ignore'
 		  entries: ['*'],
